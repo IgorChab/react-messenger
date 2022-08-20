8d5ac6f2051f8a5f28441956f3fa7bd010b54c05
IgorChab <99481845+IgorChab@users.noreply.github.com>
2022-08-20 14:36:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(errorMiddleware);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('*', function(req, res) {
+    app.get('/', function(req, res) {
       res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 }
