55e54173a380e404e79208381b160c7782a9fc37
IgorChab <chabanchukigor@gmail.com>
2022-08-20 12:10:43 +0300

    commit

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_URL = 'http://localhost:3000'
+export const API_URL = 'https://react-messenger-nodejs.herokuapp.com'
 
 const $api = axios.create({
     withCredentials: true,
--- server.js
+++ server.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'production') {
     app.get('*', function(req, res) {
       res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
-  }
+}
 
 app.use(bodyParser.json())
 app.use(cookieParser());
