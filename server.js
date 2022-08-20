e2ea17de149378db05605b5eacfefb999a010a77
IgorChab <chabanchukigor@gmail.com>
2022-08-20 13:05:52 +0300

    commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
 const path = require("path");
 const cors = require('cors');
